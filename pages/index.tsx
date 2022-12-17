accae2895522ad7ce7b3b01932fc7ddcdb41ba9f
David Veszelovszki <veszelovszki@gmail.com>
2022-12-17 09:53:25 +0100

    Split up the code, fix timing bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,184 +1,80 @@
 // @ts-ignore
 import { PocketConnector } from '../../../modules/tools/PocketConnector.ts'
-import Image from "next/image";
+import Image from "next/image"
 import styles from './index.module.scss'
-import { useEffect, useMemo, useState } from "react";
-import classnames from "classnames";
+import { useCallback, useEffect, useMemo, useState } from "react"
+import classnames from "classnames"
+import {
+    AudioSnippet,
+    getAllAudioNames,
+    getAudioNamesToPlay,
+    getTotalLength,
+    musicUrl,
+    playAudio,
+    useAllSnippets,
+    useBackgroundMusic
+} from "./audio"
+import { Character, characters, getDisplayableName, getEnabledStages, Stage, stages } from "./game"
 
-const breakBetweenAudioFilesMs = 5000;
-
-type CharacterType =
-    'doppelganger'
-    | 'werewolf'
-    | 'minion'
-    | 'mason'
-    | 'seer'
-    | 'robber'
-    | 'troublemaker'
-    | 'drunk'
-    | 'insomniac'
-    | 'villager'
-    | 'hunter'
-    | 'tanner'
-
-type Character = {
-    id: string
-    type: CharacterType
-}
-
-type StartStage = {
-    type: 'start'
-    if: 'player-count'
-    few: string
-    many: string
-    all: string
-    breaks?: boolean
-}
-
-type Stage = {
-    type: CharacterType | 'start' | 'end'
-    audioFiles: string[]
-    if?: 'minion' | 'insomniac'
-    breaks?: boolean
-} | StartStage
-
-// noinspection SpellCheckingInspection
-const characters: Character[] = [
-    { id: 'doppelganger', type: 'doppelganger' },
-    { id: 'werewolf', type: 'werewolf' },
-    { id: 'werewolf_2', type: 'werewolf' },
-    { id: 'minion', type: 'minion' },
-    { id: 'mason', type: 'mason' },
-    { id: 'mason_2', type: 'mason' },
-    { id: 'seer', type: 'seer' },
-    { id: 'robber', type: 'robber' },
-    { id: 'troublemaker', type: 'troublemaker' },
-    { id: 'drunk', type: 'drunk' },
-    { id: 'insomniac', type: 'insomniac' },
-    { id: 'villager', type: 'villager' },
-    { id: 'villager_2', type: 'villager' },
-    { id: 'villager_3', type: 'villager' },
-    { id: 'villager_4', type: 'villager' },
-    { id: 'villager_5', type: 'villager' },
-    { id: 'hunter', type: 'hunter' },
-    { id: 'tanner', type: 'tanner' },
-]
-
-const stages: Stage[] = [
-    //{ type: 'start', if: 'player-count', few: '0-start-few', many: '0-start-many', all: '0-start-all' },
-    { type: 'doppelganger', audioFiles: ['1-doppelganger'] },
-    { type: 'doppelganger', if: 'minion', audioFiles: ['2-doppelganger-1', '2-doppelganger-2', '2-doppelganger-3'] },
-    { type: 'werewolf', audioFiles: ['3-werewolf-1', '3-werewolf-2'] },
-    { type: 'minion', audioFiles: ['4-minion-1', '4-minion-2'] },
-    { type: 'mason', audioFiles: ['5-mason-1', '5-mason-2'] },
-    { type: 'seer', audioFiles: ['6-seer-1', '6-seer-2'] },
-    { type: 'robber', audioFiles: ['7-robber-1', '7-robber-2'] },
-    { type: 'troublemaker', audioFiles: ['8-troublemaker-1', '8-troublemaker-2'] },
-    { type: 'drunk', audioFiles: ['9-drunk-1', '9-drunk-2'] },
-    { type: 'insomniac', audioFiles: ['10-insomniac-1', '10-insomniac-2'] },
-    { type: 'doppelganger', if: 'insomniac', audioFiles: ['11-doppelganger-1', '11-doppelganger-2'] },
-    { type: 'end', audioFiles: ['12-end-1', '12-end-2'], breaks: false },
-]
-
-function getDisplayableName(type: CharacterType) {
-    return {
-        'doppelganger': 'alakváltó',
-        'werewolf': 'vérfarkas',
-        'minion': 'csatlós',
-        'mason': 'szabadkőműves',
-        'seer': 'látnok',
-        'robber': 'rabló',
-        'troublemaker': 'bajkeverő',
-        'drunk': 'részeges',
-        'insomniac': 'éjjeli bagoly',
-        'villager': 'falusi',
-        'hunter': 'vadász',
-        'tanner': 'tímár'
-    }[type]
-}
-
-function getCharacterImageUrl(character: Character) {
-    return `/werewolf-game/images/${character.id}.jpg`
-}
-
-function getAudioUrl(filename: string) {
-    return `/werewolf-game/audio/${filename}.m4a`;
-}
+const breakBetweenAudioFilesMs = 5000
 
 function Page() {
-    const [firstInteractionHappened, setFirstInteractionHappened] = useState(false);
+    const [firstInteractionHappened, setFirstInteractionHappened] = useState(false)
     const [started, setStarted] = useState(false)
     const [stageIndex, setStageIndex] = useState(0)
     const [selectedCharacters, setSelectedCharacters] = useState<Character[]>([])
 
-    const onStart = (selection: boolean[]) => {
-        setSelectedCharacters(characters.filter((_, index) => selection[index]))
-        setStarted(true);
-        setStageIndex(0);
-    };
-
-    const stageFilter = (stage: Stage) =>
-        stage.type === 'start'
-        || stage.type === 'end'
-        || (selectedCharacters.some(character => character.type === stage.type)
-            && (stage.if !== 'minion' || selectedCharacters.some(c => c.type === 'minion'))
-            && (stage.if !== 'insomniac' || selectedCharacters.some(c => c.type === 'insomniac')))
-
-    const enabledStages = stages.filter(stageFilter)
-
-    const nextStage = () => {
-        if (stageIndex < enabledStages.length - 1) {
-            setStageIndex(stage => stage + 1);
-        } else {
-            setStarted(false);
-            setStageIndex(0);
-        }
-    };
+    // Load audio snippets in the background
+    const snippets = useAllSnippets(useMemo(() => getAllAudioNames(stages), []))
 
     // Start music at first click, and loop it
-    useEffect(() => {
-        if (firstInteractionHappened) {
-            const backgroundMusic = new Audio('/werewolf-game/audio/music.mp3');
-            backgroundMusic.play().then(() => {});
-            backgroundMusic.loop = true
+    useBackgroundMusic(musicUrl, firstInteractionHappened)
+
+    const enabledStages = useMemo(() => getEnabledStages(stages, selectedCharacters), [selectedCharacters])
+    const nextStage = useCallback(() => {
+        if (stageIndex < enabledStages.length - 1) {
+            setStageIndex(i => i + 1)
+        } else {
+            setStarted(false)
+            setStageIndex(0)
         }
-    }, [firstInteractionHappened])
+    }, [stageIndex, enabledStages])
+
+    const onStart = (selection: boolean[]) => {
+        setSelectedCharacters(characters.filter((_, index) => selection[index]))
+        setStarted(true)
+        setStageIndex(0)
+    }
+
+    const currentStageSnippets = useMemo(
+        () => getAudioNamesToPlay(enabledStages[stageIndex], selectedCharacters).map(name => snippets[name]),
+        [enabledStages, stageIndex, selectedCharacters, snippets]
+    )
 
     return <div className={styles.entirePage} onClick={() => setFirstInteractionHappened(true)}>
         {(!started ?
-        <CharacterSelectionPage initialSelection={new Array(characters.length).fill(false)} onStart={onStart}/>
-        : <GamePage selectedCharacters={selectedCharacters}
-                    stage={enabledStages[stageIndex]} nextStage={nextStage}/>)}
+            <CharacterSelectionScreen
+                initialSelection={new Array(characters.length).fill(false)}
+                isReady={Object.keys(snippets).length > 0}
+                onStart={onStart}/>
+            : <GameScreen
+                audioSnippets={currentStageSnippets}
+                selectedCharacters={selectedCharacters}
+                enabledStages={enabledStages}
+                stageIndex={stageIndex}
+                nextStage={nextStage}/>)}
     </div>
 }
 
-function getAudioItemsWithDuration(stage: Stage, selectedCharacters: Character[]): Promise<{audio: HTMLAudioElement, durationMs: number}[]> {
-    const audioFiles = (stage.type === 'start') ? [selectedCharacters.length <= 6 ? (stage as StartStage).few : selectedCharacters.length < 18 ? (stage as StartStage).many : (stage as StartStage).all] : stage.audioFiles
-    const audioFilePromises = audioFiles.map(filename => getAudioWithDuration(getAudioUrl(filename)))
-    return Promise.all(audioFilePromises)
-}
 
-function getAudioWithDuration(url: string): Promise<{ audio: HTMLAudioElement, durationMs: number }> {
-    return new Promise((resolve, reject) => {
-        try {
-            const audio = new Audio();
-            audio.addEventListener('loadedmetadata', () => {
-                resolve({ audio, durationMs: audio.duration * 1000 })
-            })
-            audio.src = url
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
-
-function CharacterSelectionPage({
-                                    initialSelection,
-                                    onStart
-                                }: { initialSelection: boolean[], onStart: (selection: boolean[]) => void }) {
+function CharacterSelectionScreen({
+                                      initialSelection,
+                                      isReady,
+                                      onStart
+                                  }: { initialSelection: boolean[], isReady: boolean, onStart: (selection: boolean[]) => void }) {
     const [selection, setSelection] = useState(initialSelection)
-    return <div className={styles.game}>
-        <header></header>
+    return <div className={styles.characterSelectionScreen}>
+        <header><h1>Rettegés!! Valami címet ide</h1></header>
         <ul className={styles.characterCards}>
             {characters.map((c, i) =>
                 <CharacterCard
@@ -192,59 +88,61 @@ function CharacterSelectionPage({
                     })}/>)}
         </ul>
         <nav>
-            <button onClick={() => onStart(selection)}>Kezdjük is el akkor</button>
+            <button onClick={() => onStart(selection)} disabled={!isReady}>Kezdjük is el akkor</button>
         </nav>
     </div>
 }
 
-function GamePage({
-                      selectedCharacters,
-                      stage,
-                      nextStage
-                  }: { selectedCharacters: Character[], stage: Stage, nextStage: () => void }) {
-    // Calculate total audio length
+function GameScreen({
+                        audioSnippets,
+                        selectedCharacters,
+                        enabledStages,
+                        stageIndex,
+                        nextStage
+                    }:
+                        {
+                            audioSnippets: AudioSnippet[],
+                            selectedCharacters: Character[],
+                            enabledStages: Stage[],
+                            stageIndex: number,
+                            nextStage: () => void
+                        }) {
     const [startDateTime, setStartDateTime] = useState(new Date())
-    const [canceled, setCanceled] = useState(false)
-    useMemo(() => {
-        // noinspection BadExpressionStatementJS It's here to let React know that we depend on the value
-        stage;
-        setStartDateTime(new Date());
-    }, [stage])
-
-    const [audioItemsWithDuration, setAudioItemsWithDuration] = useState<{ audio: HTMLAudioElement, durationMs: number }[]>([])
-    const audioItems = audioItemsWithDuration.map(a => a.audio)
+    const [abortController, setAbortController] = useState<AbortController>(new AbortController())
+    const stage = enabledStages[stageIndex]
     useEffect(() => {
-        getAudioItemsWithDuration(stage, selectedCharacters).then(setAudioItemsWithDuration)
-    }, [stage, selectedCharacters])
-    const totalAudioLengthMs = audioItemsWithDuration.reduce((sum, item) => sum + item.durationMs, 0)
-    const totalLengthMs = totalAudioLengthMs + ((stage.breaks ?? true) ? 1 : 0) * breakBetweenAudioFilesMs
+        setStartDateTime(new Date())
+    }, [stageIndex])
+
+    const totalLengthMs = useMemo(
+        () => getTotalLength(audioSnippets) + ((stage.breaks ?? true) ? 1 : 0) * breakBetweenAudioFilesMs,
+        [audioSnippets, stage.breaks]
+    )
+
+    const moveToNextStage = useCallback(() => {
+        abortController.abort()
+        setAbortController(new AbortController())
+        nextStage()
+    }, [abortController, nextStage])
 
     useEffect(() => {
-        async function playAudio() {
-            for (const [index, audio] of audioItems.entries()) {
-                audio.currentTime = 0
-                await audio.play()
-                await waitToFinish(audio)
-                if (index === audioItems.length - 1) {
-                    break;
-                }
-                if ((stage.breaks ?? true)) {
-                    await wait(breakBetweenAudioFilesMs)
-                }
-            }
-        }
-
-        if (audioItems.length > 0 && !canceled) {
-            playAudio().then(nextStage)
-        }
+        playAudio(audioSnippets, stage.breaks ? breakBetweenAudioFilesMs : 0, abortController.signal).then(() => {
+            moveToNextStage()
+        })
 
         return () => {
-            audioItems.forEach(audio => audio.pause())
-        }
-    }, [audioItems, canceled, nextStage, stage.breaks])
+            audioSnippets.forEach(s => s.audio.pause())
 
-    return <div className={styles.game}>
-        <header><h1>Rettegés</h1></header>
+        }
+    }, [abortController.signal, audioSnippets, moveToNextStage, nextStage, stage.breaks])
+
+    return <div className={styles.gameScreen}>
+        <header>
+            {enabledStages.map((s, i) => {
+                return <div
+                    key={i}>{(s.type === 'start' || s.type === 'end') ? s.type : getDisplayableName(s.type)}</div>
+            })}
+        </header>
         {stage.type === 'start'
             ? <p>Kezdődik!</p>
             : (stage.type === 'end' ? <p>Vége...</p> : <ul>
@@ -254,9 +152,8 @@ function GamePage({
             </ul>)}
         <nav className={styles.timerNav}>
             <Timer totalMs={totalLengthMs} startDateTime={startDateTime}></Timer>
-            <button onClick={() => {
-                setCanceled(true)
-            }}>Ugrás a kövire</button>
+            <button onClick={moveToNextStage}>Ugrás a kövire
+            </button>
         </nav>
     </div>
 }
@@ -275,19 +172,9 @@ function Timer({ totalMs, startDateTime }: { totalMs: number, startDateTime: Dat
     </div>
 }
 
+// ss:ms
 function formatTime(ms: number): string {
-    return `${Math.floor(ms / 1000)}:${Math.floor(ms / 100) % 10}${Math.floor(ms / 10) % 10}`
-}
-
-// audio as a promise
-function waitToFinish(audio: HTMLAudioElement) {
-    return new Promise(resolve => {
-        audio.onended = resolve
-    })
-}
-
-function wait(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return `${Math.floor(ms / 1000)}.${Math.floor(ms / 100) % 10}${Math.floor(ms / 10) % 10}`
 }
 
 function CharacterCard({
@@ -306,9 +193,14 @@ function CharacterCard({
     </li>
 }
 
+function getCharacterImageUrl(character: Character): string {
+    return `/werewolf-game/images/${character.id}.jpg`
+}
+
+
 //
 // function CloseEyesInstructions() {
-//     const [playing, toggle] = useAudio(url);
+//     const [playing, toggle] = useAudio(url)
 //
 //     return <div>
 //         <h2>MINDENKI csukja be a szemét</h2>
